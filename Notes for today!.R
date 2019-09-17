d8db6eb34c901f6c43da198df892832cd458bf68
gkim65 <gracek1459@gmail.com>
2019-09-17 12:56:27 -0400

    new commit

--- Notes for today!.R
+++ Notes for today!.R
@@ -1,4 +1,15 @@
-"""STEPS:
+"""
+STEPS:
 1. Make sure to open a new Rmarkdown document
 2. Run knit documents with command shift k
+3. libraries: tidyverse, fivethirtyeight, ggplot2
+4. to grab the data: data("---")
+5. change echo to FALSE
+6. can use this for filter %in% c()
+7. ggplot
+    - put in group = ___ for separate lines
+    - color = _____
+    - labs(title ="", subtitle="")
+    - scale_color_manual(values = c("R" = "RED", "D" = "SKYBLUE"))+
+    
 """
